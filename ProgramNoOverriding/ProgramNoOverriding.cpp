0a46d2f865c71d067fec276dfdedf51b98194d7c
Amandaptr44 <amandaptr86@gmail.com>
2023-05-31 16:47:04 +0700

    menambahkan turunan class deriveredClass yang mewarisi dari baseClass

--- ProgramNoOverriding/ProgramNoOverriding.cpp
+++ ProgramNoOverriding/ProgramNoOverriding.cpp
@@ -3,7 +3,11 @@ using namespace std;
 
 class baseClass {
 public:
-	virtual void perkenalan() final {
+	virtual void perkenalan()  {
 		cout << "Hallo saya Function dari base class";
 	}
 };
+class derivedClass : public baseClass {
+public:
+	void perkenalan() {
+		
